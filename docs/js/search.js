0c020c67f25bef0cb96ef35c06fc137b9494c000
Ross Larson <zo0o0ot@gmail.com>
2017-09-17 22:56:04 -0500

    bulma theme

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,43 +1,57 @@
-var lunrIndex, pagesIndex;
-
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
+var lunrIndex,
+    $results,
+    pagesIndex;
 
 // Initialize lunrjs using our generated index file
 function initLunr() {
-    if (!endsWith(baseurl,"/")){
-        baseurl = baseurl+'/'
-    };
-
     // First retrieve the index file
-    $.getJSON(baseurl +"index.json")
+    $.getJSON("/hugo-theme-wave/bower_components/lunr.js/PagesIndex.json")
         .done(function(index) {
-            pagesIndex =   index;
+            pagesIndex = index;
+            console.log("index:", pagesIndex);
+
             // Set up lunrjs by declaring the fields we use
             // Also provide their boost level for the ranking
-            lunrIndex = new lunr.Index
-            lunrIndex.ref("uri");
-            lunrIndex.field('title', {
-                boost: 15
-            });
-            lunrIndex.field('tags', {
-                boost: 10
-            });
-            lunrIndex.field("content", {
-                boost: 5
+            lunrIndex = lunr(function() {
+                this.field("title", {
+                    boost: 10
+                });
+                this.field("tags", {
+                    boost: 5
+                });
+                this.field("content");
+
+                // ref is the result item identifier (I chose the page URL)
+                this.ref("href");
             });
 
             // Feed lunr with each file and let lunr actually index them
             pagesIndex.forEach(function(page) {
                 lunrIndex.add(page);
             });
-            lunrIndex.pipeline.remove(lunrIndex.stemmer)
         })
         .fail(function(jqxhr, textStatus, error) {
             var err = textStatus + ", " + error;
             console.error("Error getting Hugo index flie:", err);
         });
+}
+
+// Nothing crazy here, just hook up a listener on the input field
+function initUI() {
+    $results = $("#results");
+    $("#search").keyup(function() {
+        $results.empty();
+
+        // Only trigger a search when 2 chars. at least have been provided
+        var query = $(this).val();
+        if (query.length < 2) {
+            return;
+        }
+
+        var results = search(query);
+
+        renderResults(results);
+    });
 }
 
 /**
@@ -48,41 +62,41 @@ function initLunr() {
  */
 function search(query) {
     // Find the item in our index corresponding to the lunr one to have more info
+    // Lunr result: 
+    //  {ref: "/section/page1", score: 0.2725657778206127}
+    // Our result:
+    //  {title:"Page1", href:"/section/page1", ...}
     return lunrIndex.search(query).map(function(result) {
             return pagesIndex.filter(function(page) {
-                return page.uri === result.ref;
+                return page.href === result.ref;
             })[0];
         });
 }
 
+/**
+ * Display the 10 first results
+ *
+ * @param  {Array} results to display
+ */
+function renderResults(results) {
+    if (!results.length) {
+        return;
+    }
+
+    // Only show the ten first results
+    results.slice(0, 10).forEach(function(result) {
+        var $result = $("<li>");
+        $result.append($("<a>", {
+            href: result.href,
+            text: "» " + result.title
+        }));
+        $results.append($result);
+    });
+}
+
 // Let's get started
 initLunr();
-$( document ).ready(function() {
-    var horseyList = horsey($("#search-by").get(0), {
-        suggestions: function (value, done) {
-            var query = $("#search-by").val();
-            var results = search(query);
-            done(results);
-        },
-        filter: function (q, suggestion) {
-            return true;
-        },
-        set: function (value) {
-            location.href=value.uri;
-        },
-        render: function (li, suggestion) {
-            var uri = suggestion.uri.substring(1,suggestion.uri.length);
 
-            suggestion.href = baseurl + uri;
-
-            var query = $("#search-by").val();
-            var numWords = 2;
-            var text = suggestion.content.match("(?:\\s?(?:[\\w]+)\\s?){0,"+numWords+"}"+query+"(?:\\s?(?:[\\w]+)\\s?){0,"+numWords+"}");
-            suggestion.context = text;
-            var image = '<div>' + '» ' + suggestion.title + '</div><div style="font-size:12px">' + (suggestion.context || '') +'</div>';
-            li.innerHTML = image;
-        },
-        limit: 10
-    });
-    horseyList.refreshPosition();
+$(document).ready(function() {
+    initUI();
 });
